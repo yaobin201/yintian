e96478628657788858e95b42101275dcde01d52f
YAOBIN <1501209698@qq.com>
2025-09-08 15:47:29 +0800

    完成contactus

---
+++ scripts/contactus.js
@@ -0,0 +1,40 @@
+let contactSroller = null
+
+function sectionBgAni() {
+  contactSroller = ScrollTrigger.create({
+    trigger: '.section-bg',
+    start: 'top top',
+    end: 'bottom 50%',
+    scrub: true,
+    markers: true,
+    pin: true,
+  });
+}
+
+function contactListToggle() {
+  $('#contactusList .product-category-toggle').click(function(){
+    $(this).next().toggle();
+    contactSroller.refresh()
+  })
+}
+
+$(document).ready(function(){
+  sectionBgAni();
+
+  contactListToggle()
+
+
+  $('.contact-form').submit(function(e){
+    e.preventDefault();
+    $.ajax({
+      url: $(this).attr('action'),
+      type: $(this).attr('method'),
+      data: $(this).serialize(),
+      success: function(data){
+        if(data == 'success'){
+          $('.contact-form').html('<div class="alert alert-success">Message sent successfully</div>');
+        }
+      }
+    });
+  });
+});
